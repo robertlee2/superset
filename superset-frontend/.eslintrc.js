b76c7b47d1bca2a3b903fceba802b2a02330e350
Evan Rusackas <evan@rusackas.com>
2024-01-17 16:33:03 -0700

    pleas compile 🤞

--- superset-frontend/.eslintrc.js
+++ superset-frontend/.eslintrc.js
@@ -85,7 +85,7 @@ module.exports = {
           './tsconfig.json',
           './cypress-base/tsconfig.json',
           './packages/superset-ui-demo/tsconfig.json',
-          './packages/superset-ui-core/tsconfig.json',
+          // './packages/superset-ui-core/tsconfig.json',
         ],
       },
       extends: [
